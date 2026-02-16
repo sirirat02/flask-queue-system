bfedb32943331cfb6e129140b71b82fe53455e85
sirirat02 <sirirat021246@gmail.com>
2026-02-16 15:09:34 +0700

    fix db initialization

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ if not DATABASE_URL:
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
 # =========================
 # Models
 # =========================

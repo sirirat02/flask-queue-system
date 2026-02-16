74be97999427e2f615c01bfc0da94540f6276d49
sirirat02 <sirirat021246@gmail.com>
2026-02-16 16:08:44 +0700

    connect to old postgres db

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 # =========================
 # Models
 # =========================
